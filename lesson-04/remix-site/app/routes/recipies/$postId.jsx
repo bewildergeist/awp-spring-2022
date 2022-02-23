1e49b9f983c09670bef96c7a243457553ad62035
Piotr Pospiech <55554468+Piotr20@users.noreply.github.com>
2022-02-23 12:27:38 +0100

    fix

--- lesson-04/remix-site/app/routes/recipies/$postId.jsx
+++ lesson-04/remix-site/app/routes/recipies/$postId.jsx
@@ -26,16 +26,19 @@ export default function Recipie() {
   const { recipie } = useLoaderData();
 
   return (
-    <div>
-      <div className="page-header">
-        <h1>{recipie.title}</h1>
-        <Link to=".." className="btn btn-reverse">
+    <div className=" w-1/2 m-auto">
+      <div className="page-header flex justify-between items-center">
+        <h1 className="text-white text-3xl">{recipie.title}</h1>
+        <Link
+          to=".."
+          className="btn btn-block self-start p-4 bg-blue-500 text-white rounded-lg px-8 py-2 hover:bg-slate-100 hover:text-black transition-colors duration-300"
+        >
           Back
         </Link>
       </div>
-      <div className="page-content">
-        {recipie.body}
-        <ul>
+      <div className="page-content mt-6">
+        <h2>{recipie.body}</h2>
+        <ul className="mt-2">
           {recipie.ingredients.map((ingredient, key) => {
             return <li key={key}>{ingredient}</li>;
           })}
@@ -44,7 +47,10 @@ export default function Recipie() {
       <div className="page-footer">
         <form method="post">
           <input type="hidden" name="_method" value="delete" />
-          <button type="submit" className="btn btn-delete">
+          <button
+            type="submit"
+            className="btn btn-delete btn-blue bg-red-400 mt-6"
+          >
             Delete
           </button>
         </form>
--- lesson-04/remix-site/app/routes/recipies/new.jsx
+++ lesson-04/remix-site/app/routes/recipies/new.jsx
@@ -23,28 +23,41 @@ export const action = async ({ request }) => {
 export default function NewRecipie() {
   return (
     <>
-      <div className="page-header">
-        <h1>New Recipie</h1>
-        <Link to="/recipies" className="btn btn-reverse">
+      <div className="page-header w-1/2 m-auto page-header flex items-center justify-between">
+        <h1 className=" text-4xl font-semibold text-white">New Recipie</h1>
+        <Link
+          to="/recipies"
+          className="p-4 bg-blue-500 text-white rounded-lg px-8 py-2 hover:bg-slate-100 hover:text-black transition-colors duration-300"
+        >
           Back
         </Link>
       </div>
-      <div className="page-content">
-        <form method="POST">
-          <div className="form-control">
-            <label htmlFor="title">Title</label>
-            <input type="text" name="title" id="title" />
+      <div className="page-content w-1/2 m-auto">
+        <form method="POST" className="flex flex-col w-2/3">
+          <div className="form-control flex  justify-between py-4 flex-col">
+            <label htmlFor="title" className=" text-left text-xl">
+              Title
+            </label>
+            <input className=" h-8" type="text" name="title" id="title" />
           </div>
-          <div className="form-control">
+          <div className="form-control flex py-2 flex-col">
             <label htmlFor="ingredients">Recipie ingredients</label>
-            <input type="text" name="ingredients" id="ingredients" />
+            <input
+              className=" h-8"
+              type="text"
+              name="ingredients"
+              id="ingredients"
+            />
           </div>
-          <div className="form-control">
+          <div className="form-control flex justify-between py-2 flex-col">
             <label htmlFor="body">Recipie body</label>
             <textarea name="body" id="body"></textarea>
           </div>
 
-          <button className="btn btn-block" type="submit">
+          <button
+            className="btn btn-block mt-4 self-start p-4 bg-blue-500 text-white rounded-lg px-8 py-2 hover:bg-slate-100 hover:text-black transition-colors duration-300"
+            type="submit"
+          >
             Add Recipie
           </button>
         </form>
