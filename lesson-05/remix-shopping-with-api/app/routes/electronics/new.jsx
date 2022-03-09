563b3effa5d1cd1f9b43592f403f3816c185ca5d
WiktorrWie <eaawiwi@students.eaaa.dk>
2022-03-09 11:08:20 +0100

    v3

--- lesson-05/remix-shopping-with-api/app/routes/electronics/new.jsx
+++ lesson-05/remix-shopping-with-api/app/routes/electronics/new.jsx
@@ -1,4 +1,4 @@
-import { Link, redirect } from "remix";
+import { Form, redirect, useActionData, json  } from "remix";
 import Button from "~/components/Button.jsx";
 import PageHeader from "~/components/PageHeader";
 import Breadcrumb from "~/components/Breadcrumb.jsx";
@@ -10,6 +10,18 @@ export const action = async ({ request }) => {
   const description = form.get("description");
   const image = form.get("image");
   const uuid = new Date().getTime().toString(16);
+
+  const errors = {};
+  if (!title) errors.title = "title required";
+  if (!description) errors.description = true;
+  if (!image) errors.image = true;
+
+  if (Object.keys(errors).length) {
+    const values = Object.fromEntries(form);
+    return json({ errors, values });
+  }
+
+
   // TODO: Make a POST request via fetch to an API route that receives JSON data
   // and creates the product in the db
   await fetch("http://localhost:3000/api/electronics/", {
@@ -22,32 +34,56 @@ export const action = async ({ request }) => {
   return redirect(`/electronics/${uuid}`);
 };
 
+
 export default function NewProduct() {
+  const actionData = useActionData();
   return (
     <>
       <Breadcrumb links={[{ to: "/eletronics", title: "Electronics" }]} />
       <PageHeader title="New product" subtitle="Make it a good one" />
       <div>
-        <form method="post" className="w-64">
-          <Label htmlFor="title">Title</Label>
+        <Form reloadDocument method="post" className="w-64">
+          <Label required htmlFor="title">Title</Label>
           <input
             type="text"
             name="title"
             id="title"
             className="border p-1 border-gray-200 w-full"
+            defaultValue={actionData?.values.title}
           />
+                {actionData?.errors.title ? (
+        <p style={{ color: "red" }}>
+          {actionData.errors.title}
+        </p>
+      ) : null}
           <Label htmlFor="description">Description</Label>
           <textarea
             name="description"
             id="description"
             className="border p-1 border-gray-200 w-full"
+            defaultValue={actionData?.values.description}
           ></textarea>
+                {actionData?.errors.description ? (
+        <p style={{ color: "red" }}>
+          {actionData.errors.description}
+        </p>
+      ) : null}
           <Label htmlFor='image'>image link</Label>
-          <input type='text' name='image' id='image'></input>
+          <input
+            type='text'
+            name='image'
+            id='image'
+            defaultValue={actionData?.values.image}
+          ></input>
+                {actionData?.errors.image ? (
+        <p style={{ color: "red" }}>
+          {actionData.errors.image}
+        </p>
+      ) : null}
           <div className="mt-3">
             <Button type="submit">Add product</Button>
           </div>
-        </form>
+        </Form>
       </div>
     </>
   );
